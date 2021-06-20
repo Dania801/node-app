ef4fcef914ef1ddd5ed595cc3c73496476343ab7
Dania <dania@Danias-MacBook-Pro.local>
2021-06-20 20:23:09 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = new express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
+
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
