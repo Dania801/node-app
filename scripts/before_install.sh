5766fe35455e7771a3d8ae8cf20649e4647a2894
Dania <dania@Danias-MacBook-Pro.local>
2021-06-27 20:08:53 +0300

    Stop hooks

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-sudo apt -y install npm
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/node-app"
